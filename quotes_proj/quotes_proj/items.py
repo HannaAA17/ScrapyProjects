bb57f94b05b6a40bd012184f8f74be03d01d4d29
HannaAA17 <alberto.adell17@gmail.com>
2022-06-18 00:08:34 +0000

    added items to the quotes_proj - still to continue with the database (orm)

--- quotes_proj/quotes_proj/items.py
+++ quotes_proj/quotes_proj/items.py
@@ -8,5 +8,7 @@ import scrapy
 
 class QuotesProjItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    text = scrapy.Field()
+    author = scrapy.Field()
+    about = scrapy.Field()
+    tags = scrapy.Field()
--- quotes_proj/quotes_proj/spiders/quotes_spider.py
+++ quotes_proj/quotes_proj/spiders/quotes_spider.py
@@ -1,5 +1,6 @@
 
 import scrapy
+from ..items import QuotesProjItem
 
 class QuotesSpiderSpider(scrapy.Spider):
     name = 'quotes_spider'
@@ -7,12 +8,12 @@ class QuotesSpiderSpider(scrapy.Spider):
 
     def parse(self, response):
         for quote in response.xpath('//div[@class="quote"]'):
-            yield {
+            yield QuotesProjItem(**{
                 'text':quote.xpath('.//*[@class="text"]/text()').get().strip(u'\u201c\u201d'), #strip unicode chars u'\u201c\u201d
                 'author':quote.xpath('.//*[@class="author"]/text()').get().strip(),
                 'about':quote.xpath('.//a[@href]/@href').get().strip(),
                 'tags':quote.xpath('.//*[@class="tag"]/text()').getall(),
-            }
+            })
         
         next_page_url = response.urljoin(response.xpath('.//li[@class="next"]/a/@href').get())
         
